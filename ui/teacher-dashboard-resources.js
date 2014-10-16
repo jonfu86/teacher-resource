d9dfad58d684de820f29d23b8ce653608251c585
jonfu86 <noreply>
2014-10-16 19:54:42 +0000

    Adding files

---
+++ ui/teacher-dashboard-resources.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/teacher-dashboard-resources.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TeacherDashboardResources
+ * @extends Component
+ */
+exports.TeacherDashboardResources = Component.specialize(/** @lends TeacherDashboardResources# */ {
+    constructor: {
+        value: function TeacherDashboardResources() {
+            this.super();
+        }
+    }
+});
