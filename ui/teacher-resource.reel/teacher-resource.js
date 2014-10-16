12c8449dbfdf05ca6c11308e2f91f09ac310faad
jonfu86 <noreply>
2014-10-16 20:25:29 +0000

    Add component teacher-resource

---
+++ ui/teacher-resource.reel/teacher-resource.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/teacher-resource.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TeacherResource
+ * @extends Component
+ */
+exports.TeacherResource = Component.specialize(/** @lends TeacherResource# */ {
+    constructor: {
+        value: function TeacherResource() {
+            this.super();
+        }
+    }
+});
